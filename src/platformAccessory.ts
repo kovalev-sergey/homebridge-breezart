3a69cd45fef5795a2594d9c536295e59e9d2e621
Sergey Kovalev <sergey@weedudidie.com>
2021-01-13 11:17:12 +0300

    chore: cleaning

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -6,6 +6,11 @@ import { BreezartHomebridgePlatform } from './platform';
 import BreezartClient from 'breezart-client';
 
 /**
+ * Device polling interval in mc
+ */
+const POLLS_INTERVAL = 1000;
+
+/**
  * Breezart Accessory
  * An instance of this class is created for each accessory your platform registers
  * Each accessory may expose multiple services of different service types.
@@ -84,128 +89,9 @@ export class BreezartPlatformAccessory {
     this.fService.getCharacteristic(this.platform.Characteristic.FilterLifeLevel)
       .on('get', this.getFilterLifeLevel.bind(this));
     // TODO: Reset filter indications https://developers.homebridge.io/#/characteristic/ResetFilterIndication
-      
-    
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    // this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    // this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
-
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
-
-    // register handlers for the On/Off Characteristic
-    // this.service.getCharacteristic(this.platform.Characteristic.On)
-    //   .on('set', this.setOn.bind(this))                // SET - bind to the `setOn` method below
-    //   .on('get', this.getOn.bind(this));               // GET - bind to the `getOn` method below
-
-    // register handlers for the Brightness Characteristic
-    // this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-    //   .on('set', this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
-
-
-    /**
-     * Creating multiple services of the same type.
-     * 
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     * 
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    // const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    // const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-    //   this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     * 
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     * 
-     */
     this.initBreezartClient(accessory.context.device);
-
-
-    // let motionDetected = false;
-    // setInterval(() => {
-    //   // EXAMPLE - inverse the trigger
-    //   motionDetected = !motionDetected;
-
-    //   // push the new value to HomeKit
-    //   motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-    //   motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-    //   this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-    //   this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    // }, 100000);
   }
-
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
-   */
-  // setOn(value: CharacteristicValue, callback: CharacteristicSetCallback) {
-
-  //   // implement your own code to turn your device on/off
-  //   this.deviceStates.On = value as boolean;
-
-  //   this.platform.log.debug('Set Characteristic On ->', value);
-
-  //   // you must call the callback function
-  //   callback(null);
-  // }
-
-  /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   * 
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   * 
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
-   */
-  // getOn(callback: CharacteristicGetCallback) {
-
-  //   // implement your own code to check if the device is on
-  //   const isOn = this.deviceStates.On;
-
-  //   this.platform.log.debug('Get Characteristic On ->', isOn);
-
-  //   // you must call the callback function
-  //   // the first argument should be null if there were no errors
-  //   // the second argument should be the value to return
-  //   callback(null, isOn);
-  // }
-
-  /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
-   */
-  // setBrightness(value: CharacteristicValue, callback: CharacteristicSetCallback) {
-
-  //   // implement your own code to set the brightness
-  //   this.deviceStates.Brightness = value as number;
-
-  //   this.platform.log.debug('Set Characteristic Brightness -> ', value);
-
-  //   // you must call the callback function
-  //   callback(null);
-  // }
 
   getActive(callback: CharacteristicGetCallback) {
     // Min Value	0
@@ -382,7 +268,7 @@ export class BreezartPlatformAccessory {
     // then Breezart was connected 
     this.breezart.on('connect', () => {
       this.platform.log.debug('Connection established', options.name);
-      this.startPolls();
+      this.startPolls(POLLS_INTERVAL);
     });
     // if device disconnected - reconnect after 5 sec
     this.breezart.on('disconnect', () => {
@@ -404,7 +290,7 @@ export class BreezartPlatformAccessory {
   /**
    * Start periodical polls of the device for reading characteristics
    */
-  startPolls() {
+  startPolls(interval) {
     setInterval(() => {
       if (!this.breezart.connected) {
         return;
@@ -412,7 +298,7 @@ export class BreezartPlatformAccessory {
       this.breezart.getCurrentStatus(() => {
         this.setDeviceStates();
       });
-    }, 1000);
+    }, interval);
   }
 
   setDeviceStates() {
